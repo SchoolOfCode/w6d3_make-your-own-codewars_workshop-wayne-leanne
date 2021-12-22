607848a70484eb8c19a1035e67379e2b58475832
Leanne251 <leannesmith251@gmail.com>
2021-12-22 15:46:15 +0000

    start of task

--- main.js
+++ main.js
@@ -2,4 +2,13 @@
 👉 Write your kata here!
 */
 
+// take an array of words, and create a new array where the words only start & finish with vowel. 
+
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+export default function sortList(){
+    // Your function goes here.
+}
+
+  
--- main.test.js
+++ main.test.js
@@ -1,1 +1,22 @@
 //👉 Write your tests below here:
+
+import sortList from "./main.js";
+
+test(`Should return an array where all words start & finish with a vowel`, function(){
+    
+    //arrange
+    const input = ["Australia", "Thailand", "Africa", "Bali", "India", "England", "America", "Asia",]
+    // act
+    const actual = sortList(input);
+    //assert
+    const expected = ["Australia", "Africa", "India", "America", "Asia",]
+
+    expect(actual).toStrictEqual(expected);
+  
+})
+
+
+
+
+
+
